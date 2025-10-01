4edfbf6126728db185a596488e28a44e478ce077
LeeChangMin <77597971+changmin2@users.noreply.github.com>
2025-10-01 23:30:05 +0900

    init

---
+++ src/main/java/com/daldal/schetalk/SchetalkApplication.java
@@ -0,0 +1,13 @@
+package com.daldal.schetalk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchetalkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchetalkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/daldal/schetalk/SchetalkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.daldal.schetalk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchetalkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
